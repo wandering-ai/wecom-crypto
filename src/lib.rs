63eb0d2b793b46d956144eaf31abbf6b857c0aab
yinguobing <yinguobing@gmail.com>
2024-04-06 16:50:17 +0800

    add custom error type

--- src/lib.rs
+++ src/lib.rs
@@ -29,11 +29,24 @@ use aes::{
 use base64::{alphabet, engine, Engine as _};
 use cbc::{Decryptor, Encryptor};
 use sha1::{Digest, Sha1};
-use std::error::Error;
+use thiserror::Error;
+
+/// 加解密过程中可能出现的错误
+#[derive(Error, Debug)]
+pub enum CryptoError {
+    #[error("Wecom decode error. {0}")]
+    WecomDecode(String),
+    #[error("Base64 decode error. {0}")]
+    Base64Decode(#[from] base64::DecodeError),
+    #[error("UTF-8 decode error. {0}")]
+    Utf8Decode(#[from] std::string::FromUtf8Error),
+    #[error("AES decryption error. {0}")]
+    AesDecryption(String),
+}
 
 /// 生成数据签名，用于校验请求数据是否被篡改。输入需要包含Token。
-pub fn generate_signature(inputs: Vec<&str>) -> String {
-    let mut content = inputs.clone();
+pub fn generate_sha1_signature(inputs: &[&str]) -> String {
+    let mut content = inputs.to_vec();
     content.sort_unstable();
     let digest = Sha1::digest(content.concat().as_bytes());
     base16ct::lower::encode_string(&digest)
@@ -48,11 +61,14 @@ pub struct Source {
     pub receive_id: String,
 }
 
+// AES解密常量化区块大小
+const AES_BLOCK_SIZE: usize = 32;
+
 /// 加解密功能代理。是加解密方法的数据结构载体。
 #[derive(Clone)]
 pub struct Agent {
     token: String,
-    key: [u8; 32],
+    key: [u8; AES_BLOCK_SIZE],
     nonce: [u8; 16],
 }
 
@@ -68,7 +84,8 @@ impl Agent {
         let key_as_vec = engine::GeneralPurpose::new(&alphabet::STANDARD, config)
             .decode(key)
             .expect("AES key should be valid Base64 string");
-        let key = <[u8; 32]>::try_from(key_as_vec).expect("AES key length should be 32");
+        let key =
+            <[u8; AES_BLOCK_SIZE]>::try_from(key_as_vec).expect("AES key length should be 32");
         let nonce = <[u8; 16]>::try_from(&key[..16]).unwrap();
         Self {
             token: token.to_owned(),
@@ -84,14 +101,14 @@ impl Agent {
     ///
     /// let agent = Agent::new("a", "cGCVnNJRgRu6wDgo7gxG2diBovGnRQq1Tqy4Rm4V4qF");
     /// assert_eq!(
-    ///     agent.generate_signature(vec!["0", "c", "b"]),
+    ///     agent.generate_signature(&["0", "c", "b"]),
     ///     "a8addbc99f8b3f51d2adbceb605d650b9a8940e2"
     /// )
     /// ```
-    pub fn generate_signature(&self, inputs: Vec<&str>) -> String {
-        let mut content = inputs.clone();
+    pub fn generate_signature(&self, inputs: &[&str]) -> String {
+        let mut content = inputs.to_vec();
         content.push(&self.token);
-        generate_signature(content)
+        generate_sha1_signature(&content)
     }
 
     /// 加密给定的数据结构体。加密后的字符串为BASE64编码后的数据。
@@ -118,18 +135,27 @@ impl Agent {
     }
 
     /// 解密BASE64编码的加密数据。解密后的数据为Source类型。
-    pub fn decrypt(&self, encoded: &str) -> Result<Source, Box<dyn Error + Send + Sync>> {
-        let cipher_bytes = engine::general_purpose::STANDARD
-            .decode(encoded)
-            .map_err(|e| format!("Base64 decode error: {}", e))?;
+    pub fn decrypt(&self, encoded: &str) -> Result<Source, CryptoError> {
+        // Base64解码
+        let cipher_bytes = engine::general_purpose::STANDARD.decode(encoded)?;
+        // AES解密
         let block = Decryptor::<Aes256>::new(&self.key.into(), &self.nonce.into())
             .decrypt_padded_vec_mut::<NoPadding>(&cipher_bytes)
-            .map_err(|e| format!("AES decryption error: {}", e))?;
-        let padding_size: usize = block.last().copied().unwrap() as usize;
-        let buf = &block[..block.len() - padding_size];
-        let msg_len: usize = u32::from_be_bytes(buf[16..20].try_into().unwrap()) as usize;
-        let text = String::from_utf8(buf[20..20 + msg_len].to_vec())?;
-        let receive_id = String::from_utf8(buf[20 + msg_len..].to_vec())?;
+            .map_err(|e| CryptoError::AesDecryption(e.to_string()))?;
+        // 获取填充长度与消息长度
+        let Some(padding_size) = block.last().copied() else {
+            return Err(CryptoError::WecomDecode(
+                "Failed to get padding size, empty block".to_string(),
+            ));
+        };
+        let Ok(msg_len_bytes): Result<[u8; 4], _> = block[16..20].try_into() else {
+            return Err(CryptoError::WecomDecode("Invalid message size".to_string()));
+        };
+        // 提取消息
+        let msg_len = u32::from_be_bytes(msg_len_bytes) as usize;
+        let text = String::from_utf8(block[20..20 + msg_len].to_vec())?;
+        let receive_id =
+            String::from_utf8(block[20 + msg_len..block.len() - padding_size as usize].to_vec())?;
         Ok(Source { text, receive_id })
     }
 }
@@ -143,7 +169,7 @@ mod tests {
         let key = "cGCVnNJRgRu6wDgo7gxG2diBovGnRQq1Tqy4Rm4V4qF";
         let agent = Agent::new(token, key);
         assert_eq!(
-            agent.generate_signature(vec!["0", "c", "b"]),
+            agent.generate_signature(&["0", "c", "b"]),
             "a8addbc99f8b3f51d2adbceb605d650b9a8940e2",
         );
     }
@@ -152,7 +178,7 @@ mod tests {
     fn test_mod_signature() {
         let token = "a";
         assert_eq!(
-            super::generate_signature(vec![token, "0", "c", "b"]),
+            super::generate_sha1_signature(&[token, "0", "c", "b"]),
             "a8addbc99f8b3f51d2adbceb605d650b9a8940e2",
         );
     }
